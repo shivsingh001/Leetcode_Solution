7f25ab43dc57d02967718c130070a2367a7c61f1
Shivam Kumar Singh <shivam.kumar@gmail.com>
2023-12-29 16:28:49 +0530

    Hashmap

--- Ace Coding Interview with 75 Qs/Hashmap/2215. Find the Difference of Two Arrays.cpp
+++ Ace Coding Interview with 75 Qs/Hashmap/2215. Find the Difference of Two Arrays.cpp
@@ -1,9 +1,12 @@
+// if you like this code please upvote this
+
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
         unordered_set<int> set1(nums1.begin(), nums1.end());
         unordered_set<int> set2(nums2.begin(), nums2.end());
-        
+        int valuu= 0;
+        int dist=0;
         vector<int> distinct_nums1, distinct_nums2;
         for (int num : set1) {
             if (set2.count(num) == 0) {
