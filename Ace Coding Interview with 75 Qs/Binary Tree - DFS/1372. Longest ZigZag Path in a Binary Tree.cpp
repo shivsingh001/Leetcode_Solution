4923ae571bae070e285a67f8846f10ad44ffc565
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-04 22:58:53 +0530

    Binary Tree

---
+++ Ace Coding Interview with 75 Qs/Binary Tree - DFS/1372. Longest ZigZag Path in a Binary Tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+    int maxLength=0;
+
+    void solve(TreeNode* root,int dir,int currLength){
+        if(!root) return;
+        maxLength=max(maxLength,currLength);
+        solve(root->left,0,dir?currLength+1:1);
+        solve(root->right,1,dir?1:currLength+1);
+    }
+
+    int longestZigZag(TreeNode* root) {
+        solve(root,0,0);
+        solve(root,1,0);
+        return maxLength;
+    }
+};
