b25f58011f704aa50e7758d892a990dd694c1346
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-17 22:08:17 +0530

    Updated the Code

--- Ace Coding Interview with 75 Qs/Array and Strings/238. Product of Array Except Self.cpp
+++ Ace Coding Interview with 75 Qs/Array and Strings/238. Product of Array Except Self.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        int fromBegin=1;
-        int fromLast=1;
+        int fromBegin=1; // pointer to traverse from the begninng.
+        int fromLast=1;  // pointer to traverse from the end.
         vector<int> res(n,1);
         
         for(int i=0;i<n;i++){
-            res[i]*=fromBegin;
+            res[i]*=fromBegin; // storing the value except the position from start
             fromBegin*=nums[i];
-            res[n-1-i]*=fromLast;
+            res[n-1-i]*=fromLast; //storing values except the position from the end 
             fromLast*=nums[n-1-i];
         }
         return res;
