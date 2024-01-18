a820a65ef92736a19c5fa8e7ac041e721e2f835b
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-18 22:41:44 +0530

    Redefining the code

--- Ace Coding Interview with 75 Qs/Heap/215. Kth Largest Element in an Array.cpp
+++ Ace Coding Interview with 75 Qs/Heap/215. Kth Largest Element in an Array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>p;
-        for(auto x:nums){
+        priority_queue<int,vector<int>,greater<int>>p; //defining the min heap
+        for(auto x:nums){   // traversing the whole array
             p.push(x);
             if(p.size()>k){
                 p.pop();
